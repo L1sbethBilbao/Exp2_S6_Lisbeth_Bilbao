5bdab745f163b055dad461dcf2d98944d1ab16e9
L1sbethBilbao <lisbethbilbao1@gmail.com>
2025-04-21 20:17:23 -0400

    se agrega version

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-
+// version 6.0
 public class JavaApplication7 {
 
     static Scanner sc = new Scanner(System.in);
